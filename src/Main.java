494cd4ea8f7d12966776fac58374c1bc932fc38c
Alexis <alegarpov@alu.edu.gva.es>
2024-12-03 20:31:13 +0100

    Modificación del repositorio

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        System.out.println("Repositorio modificado");
+        System.out.println("Repositorio modificado con éxito");
 
     }
 }
